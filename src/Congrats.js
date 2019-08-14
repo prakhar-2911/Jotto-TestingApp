9a91bb1d050cfe3e3094820272423068f4a5531b
Prakhar <prakharcsharma@gmail.com>
2019-08-14 15:10:33 +0530

    Completed Congrats Component

---
+++ src/Congrats.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
+};
