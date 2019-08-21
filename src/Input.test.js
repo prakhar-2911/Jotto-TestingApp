b45f7ad3db28186bb945b40f64eabcc9566c3541
Prakhar <prakharcsharma@gmail.com>
2019-08-21 23:57:34 +0530

    Input.js tests added

---
+++ src/Input.test.js
@@ -0,0 +1,46 @@
+import {shallow} from 'enzyme';
+import React from 'react';
+import {findByTestAttr, storeFactory} from '../test/testUtils';
+import Input from './Input';
+
+
+const setup = (initialState={}) => {
+const store = storeFactory(initialState);
+const wrapper = shallow(<Input store={store}/>).dive().dive();
+console.log(wrapper.debug());
+};
+
+setup();
+
+describe('render', () => {
+    describe('word has not been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+
+        test('renders input box', () => {
+
+        });
+
+        test('renders submit button', () => {
+            
+        });
+    })
+
+    describe('word has been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+
+        test('does not renders input box', () => {
+
+        });
+
+        test('does not renders submit button', () => {
+            
+        });
+    });
+});
+
+describe('update state', () => {
+})
