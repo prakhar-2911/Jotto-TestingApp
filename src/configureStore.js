71fcb802b797379bc0cea9a7761d19d228f60571
Prakhar <prakharcsharma@gmail.com>
2019-08-30 11:34:03 +0530

    Install redux thunk middleware

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,4 +1,8 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import ReduxThunk from 'redux-thunk'
 import rootReducer from './reducers/index';
+
+
+export const middlewares = [ReduxThunk];
 
 export default createStore(rootReducer);
